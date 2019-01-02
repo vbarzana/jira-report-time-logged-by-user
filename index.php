7955e089fceb73a1ebf8d154bed4abc49cb94123
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2019-01-02 10:51:42 +0200

    [feature] Enhancing the worklogs to show a cleaner design and more details of what actually was in the description of every worklog.

--- index.php
+++ index.php
@@ -73,14 +73,14 @@ function buildRowFromData($issues)
     $arr = [];
     foreach ($issues as $i => $issue) {
         $field = $issue['fields'];
-        $arr[$i]['key'] = $issue['key'];
+        $worklogs = getWorklogs($issue['key'], $field['status']['name']);
+        $arr[$i]['key'] = '<a href="' . getBrowseUrlForIssue($issue['key']) . '" target="_blank">' . $issue['key'] . '</a>';
         $arr[$i]['assignee'] = $field['assignee']['displayName'];
         $arr[$i]['status'] = $field['status']['name'];
         $arr[$i]['priority'] = $field['priority']['name'];
-        $arr[$i]['summary'] = getWorklogs($issue['key'], $field['status']['name']);
+        $arr[$i]['summary'] = '<strong style="font-size: 12px;">' . $field['summary'] . '</strong><br/>' . $worklogs;
         $arr[$i]['total_time_spent'] = ($field['timespent'] / 3600);
     }
-
     return $arr;
 }
 
@@ -95,26 +95,34 @@ function debug($data)
 function getWorklogs($key, $status)
 {
     global $cfg;
+
     $statusCls = str_replace(" ", "", strtolower($status));
-    $url = getBaseUrl() . "issue/$key/worklog?jql=" . urlencode("worklogAuthor=" . $cfg['jira_username'] . " AND worklogDate >= " . $cfg['from'] . "AND worklogDate <= " . $cfg['to']);
+    $url = getApiBaseUrl() . "issue/$key/worklog?jql=" . urlencode("worklogAuthor=" . $cfg['jira_username'] . " AND worklogDate >= " . $cfg['from'] . "AND worklogDate <= " . $cfg['to']);
     $worklogData = getData($url);
     $worklogs = json_decode($worklogData, true);
     $comments = '';
     foreach ($worklogs['worklogs'] as $i => $worklog) {
         $date = $date = new DateTime($worklog['started']);
         $date = $date->format('Y-m-d H:i:s');
-        $comments .= '<div class="entry' . ($i % 2 == 0 ? ' striped' : '') . '"><span class="date ' . $statusCls . '">' . $date . ' (' . $worklog['timeSpent'] . ')</span><span class="description">' . $worklog['comment'] . '</span></div>';
+        $comment = str_replace('\n', '<br/>', $worklog['comment']);
+        $comments .= '<div class="entry' . ($i % 2 == 0 ? ' striped' : '') . '"><span class="date ' . $statusCls . '">' . $date . ' (' . $worklog['timeSpent'] . ')</span><span class="description">' . $comment . '</span></div>';
     }
     return $comments;
 }
 
-function getBaseUrl()
+function getApiBaseUrl()
 {
     global $cfg;
     return $cfg['jira_host_address'] . "/rest/api/2/";
 }
 
-function getIssuesUrl()
+function getBrowseUrlForIssue($issue)
+{
+    global $cfg;
+    return $cfg['jira_host_address'] . '/browse/' . ($issue ? $issue : '');
+}
+
+function getApiIssuesUrl()
 {
     global $cfg;
     $jiraKey = $_POST["jira_key"];
@@ -125,25 +133,25 @@ function getIssuesUrl()
     $jiraKey = strtoupper($jiraKey);
     // load url
     $jql = urlencode("project=" . $jiraKey . " AND worklogAuthor=" . $cfg['jira_username'] . " AND worklogDate >= " . $cfg['from'] . "AND worklogDate <= " . $cfg['to']);
-    return getBaseUrl() . "search?jql=" . $jql . "&maxResults=" . $cfg['max_results'];
+    return getApiBaseUrl() . "search?jql=" . $jql . "&maxResults=" . $cfg['max_results'];
 }
 
 if (!empty($_POST)) {
     if ($_POST["submit"] === "fetch") {
-        $url = getIssuesUrl();
+        $url = getApiIssuesUrl();
 
         $result = getData($url);
         $decodedData = json_decode($result, true);
         $issues = $decodedData['issues'];
         $rows = buildRowFromData($issues);
         $total = 0;
-        foreach ($rows as &$row) {
+        foreach ($rows as $i => $row) {
             $total += $row['total_time_spent'];
-            $row['total_time_spent'] = $row['total_time_spent'] . ' h';
         }
         $totalRow = array();
         $totalRow['key'] = '<b>TOTAL: </b>';
-        $totalRow['total_time_spent'] = '<b>' . $total . ' h</b>';
+        $totalRow['total_row'] = true;
+        $totalRow['total_time_spent'] = $total;
 
         array_push($rows, $totalRow);
 
@@ -162,149 +170,106 @@ if (!empty($_POST)) {
     }
 }
 ?>
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>JIRA Export</title>
-    <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css"
-          integrity="sha384-UQiGfs9ICog+LwheBSRCt1o5cbyKIHbwjWscjemyBMT9YCUMZffs6UqUTd0hObXD" crossorigin="anonymous">
-    <style>
-        body {
-            font-family: Arial;
-            padding: 30px
-        }
-
-        .entry {
-            margin: 0 0 5px 0;
-            float: left;
-            width: 100%;
-            -webkit-border-radius: 3px;
-            -moz-border-radius: 3px;
-            border-radius: 3px;
-        }
-
-        .entry span {
-            float: left;
-            margin-left: 5px;
-        }
-
-        .entry.striped {
-            background-color: rgba(240, 248, 255, 0.41);
-        }
-
-        span.date {
-            color: #594300;
-            background-color: #d3efff;
-            border: 1px solid #d4d4d4;
-            padding: 3px 5px 2px 5px;
-            min-width: 76px;
-            border-radius: 3px;
-            display: inline-block;
-            font-size: 11px;
-            line-height: 99%;
-            margin: 0;
-            text-align: center;
-            text-transform: uppercase;
-        }
-
-        span.date.inprogress {
-            background-color: #fbff5d;
-        }
-
-        span.description {
-            font-style: italic;
-            float: none;
-            width: auto;
-            font-size: 12px;
-            clear: left;
-        }
-
-        label {
-            margin-top: 50px;
-            width: 360px;
-            font-size: 22px;
-            font-weight: 700;
-            padding-bottom: 10px;
-        }
-
-        input {
-            margin-top: 5px;
-            height: 40px;
-            width: 400px;
-            font-size: 20px;
-            padding-left: 15px;
-            text-transform: uppercase;
-            vertical-align: bottom;
-        }
-
-        button {
-            color: #fff;
-            vertical-align: bottom;
-            height: 46px;
-        }
-
-        .button-success {
-            background: #1cb841;
-            color: #fff;
-        }
-
-        .button-secondary {
-            background: #42b8dd
-        }
-
-        .button-small {
-            font-size: 85%
-        }
-
-        .button-xlarge {
-            font-size: 125%
-        }
-    </style>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>Worklog report from Jira!</title>
 </head>
 <body>
-<?php if (!empty($error)) : ?>
-    <div>
-        <p><?php echo $error ?></p>
-    </div>
-<?php endif; ?>
 
-<form method="POST">
-    <div>
-        <label>Enter JIRA Project Key<br><input type="text" name="jira_key" placeholder="Eg. PROJ, ABC"></label>
-        <button name="submit" class="button-success button-xlarge pure-button" value="fetch">Run Report</button>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="?">Jira Worklog Report</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+
+        <form class="form-inline my-2 my-lg-0" method="POST" style="float: right;">
+            <input class="form-control mr-sm-2" type="text" name="jira_key"
+                   placeholder="<?= $cfg['project_key'] ? $cfg['project_key'] : 'PROJECT' ?>">
+            <button name="submit" class="btn btn-outline-success my-2 my-sm-0" value="fetch">Get my worklogs</button>
+            <?php if (!empty($rows)): ?>
+                <button name="submit" class="btn btn-outline-info my-2 my-sm-0" style="margin-left: 8px;"
+                        value="export">Export CSV
+                </button>
+            <?php endif; ?>
+        </form>
     </div>
+</nav>
+
+<main role="main" class="container">
+    <?php if (!empty($error)) : ?>
+        <div>
+            <p><?php echo $error ?></p>
+        </div>
+    <?php endif; ?>
 
     <?php if (!empty($rows)) : ?>
         <hr/>
-        <h3>Results
-            <button name="submit" class="button-secondary button-small pure-button" value="export">Export CSV</button>
-        </h3>
-        <table class="pure-table pure-table-bordered">
-        <thead>
-        <tr>
-            <th width="100">Key</th>
-            <th>Assignee</th>
-            <th>Status</th>
-            <th>Priority</th>
-            <th>Summary</th>
-            <th>Total Time Spent</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($rows as $index => $row) : ?>
+        <table class="table table-responsive">
+            <thead class="thead-light">
             <tr>
-                <td><?= @$row['key']; ?></td>
-                <td><?= @$row['assignee']; ?></td>
-                <td><?= @$row['status']; ?></td>
-                <td><?= @$row['priority']; ?></td>
-                <td><?= @$row['summary']; ?></td>
-                <td><?= @$row['total_time_spent']; ?></td>
+                <th scope="col">Key</th>
+                <th scope="col">Assignee</th>
+                <th scope="col">Status</th>
+                <th scope="col">Priority</th>
+                <th scope="col" class="summary">Summary</th>
+                <th scope="col">Time Spent</th>
             </tr>
-        <?php endforeach; ?>
-        </tbody>
-        </table><?php
-    endif ?>
-</form>
+            </thead>
+            <tbody>
+            <?php foreach ($rows as $index => $row) : ?>
+                <tr class="<? echo @$row['total_row'] ? 'thead-light' : '' ?>">
+                    <th scope="row" class="no-whitespace"><?= @$row['key']; ?></th>
+                    <td><?= @$row['assignee']; ?></td>
+                    <td><?= @$row['status']; ?></td>
+                    <td><?= @$row['priority']; ?></td>
+                    <td class="summary"><?= @$row['summary']; ?></td>
+                    <td><?= @$row['total_time_spent']; ?> h</td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <div class="jumbotron">
+            <h1 class="display-4">Monthly work report</h1>
+            <p class="lead">Make sure you have changed the config.json or the config.local.json files and then just
+                click "search".</p>
+            <form method="POST">
+                <div class="form-group">
+                    <input class="form-control mr-sm-2" style="max-width: 100px;float: left;" type="text"
+                           name="jira_key"
+                           placeholder="<?= $cfg['project_key'] ? $cfg['project_key'] : 'PROJECT' ?>">
+                    <button name="submit" class="btn btn-outline-success my-2 my-sm-0" value="fetch">Get my worklogs
+                    </button>
+                </div>
+            </form>
+        </div>
+    <?php endif ?>
+
+</main><!-- /.container -->
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 </body>
 </html>
